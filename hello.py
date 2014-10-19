ece35dd49a02258cffdd5e87e3943ad096093afd
mlavin <markdlavin@gmail.com>
2014-10-19 15:56:25 -0400

    Adding initial Hello World example.

---
+++ hello.py
@@ -0,0 +1,35 @@
+import sys
+
+
+from django.conf import settings
+
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='thisisthesecretkey',
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+
+from django.conf.urls import url
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello World')
+
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
