08a8a877ca9c64bfd5969d386eca38638f1a5093
Julia Elman <julia.elman@gmail.com>
2014-10-20 12:59:51 -0400

    Adding dynamic content + completing Chapter 3.

--- chapter3/sitebuilder/views.py
+++ chapter3/sitebuilder/views.py
@@ -1,9 +1,11 @@
+import json
 import os
 
 from django.conf import settings
 from django.http import Http404
 from django.shortcuts import render
-from django.template import Template
+from django.template import Template, Context
+from django.template.loader_tags import BlockNode
 from django.utils._os import safe_join
 
 def get_page_or_404(name):
@@ -18,6 +20,12 @@ def get_page_or_404(name):
    
     with open(file_path, 'r') as f:
         page = Template(f.read())
+    meta = None
+    for i, node in enumerate(list(page.nodelist)):
+        if isinstance(node, BlockNode) and node.name == 'context':
+            meta = page.nodelist.pop(i)
+            break
+    page._meta = meta
     return page
 
 
@@ -29,4 +37,8 @@ def page(request, slug='index'):
         'slug': slug,
         'page': page,
     }
+    if page._meta is not None:
+    	meta = page._meta.render(Context())
+    	extra_context = json.loads(meta)
+    	context.update(extra_context)
     return render(request, 'page.html', context)
