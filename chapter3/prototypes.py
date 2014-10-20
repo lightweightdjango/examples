f57509bfe8df5d32e2466f939a916648901252ed
Julia Elman <julia.elman@gmail.com>
2014-10-20 00:21:31 -0400

    Adding custom build command for generating static content.

--- chapter3/prototypes.py
+++ chapter3/prototypes.py
@@ -18,6 +18,8 @@ settings.configure(
     ),
     STATIC_URL='/static/',
     SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
+    SITE_OUTPUT_DIRECTORY=os.path.join(BASE_DIR, '_build'),
+    STATIC_ROOT=os.path.join(BASE_DIR, '_build', 'static'),
 )
 
 
