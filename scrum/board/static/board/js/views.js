cc689d530178e2bf5a856bfdd228ca007abd3bfc
Julia Elman <julia.elman@gmail.com>
2014-11-05 22:40:13 -0500

    Creating a generic formview template for Backbone Chapter 5 project.

--- scrum/board/static/board/js/views.js
+++ scrum/board/static/board/js/views.js
@@ -15,42 +15,18 @@
         }
     });
     
-    var HomepageView = TemplateView.extend({
-        templateName: '#home-template'
-    });
-    
-    var LoginView = TemplateView.extend({
-        id: 'login',
-        templateName: '#login-template',
-        errorTemplate: _.template('<span class="error"><%- msg %></span>'),
+    var FormView = TemplateView.extend({
         events: {
             'submit form': 'submit'
         },
-        submit: function (event) {
-            var data = {};
-            event.preventDefault();
-            this.form = $(event.currentTarget);
-            this.clearErrors();
-            data = {
-                username: $(':input[name="username"]', this.form).val(),
-                password: $(':input[name="password"]', this.form).val()
-            };
-            $.post(app.apiLogin, data)
-                .success($.proxy(this.loginSuccess, this))
-                .fail($.proxy(this.loginFailure, this));
-        },
-        loginSuccess: function (data) {
-            app.session.save(data.token);
-            this.trigger('login', data.token);
-        },
-        loginFailure: function (xhr, status, error) {
-            var errors = xhr.responseJSON;
-            this.showErrors(errors);
+        errorTemplate: _.template('<span class="error"><%- msg %></span>'),
+        clearErrors: function () {
+            $('.error', this.form).remove();
         },
         showErrors: function (errors) {
             _.map(errors, function (fieldErrors, name) {
                 var field = $(':input[name=' + name + ']', this.form),
-                label = $('label[for=' + field.attr('id') + ']', this.form);
+                    label = $('label[for=' + field.attr('id') + ']', this.form);
                 if (label.length === 0) {
                     label = $('label', this.form).first();
                 }
@@ -60,8 +36,48 @@
                 _.map(fieldErrors, appendError, this);
             }, this);
         },
-        clearErrors: function () {
-            $('.error', this.form).remove();
+        serializeForm: function (form) {
+            return _.object(_.map(form.serializeArray(), function (item) {
+                // Convert object to tuple of (name, value)
+                return [item.name, item.value];
+            }));
+        },
+        submit: function (event) {
+            event.preventDefault();
+            this.form = $(event.currentTarget);
+            this.clearErrors();
+        },
+        failure: function (xhr, status, error) {
+            var errors = xhr.responseJSON;
+            this.showErrors(errors);
+        },
+        done: function (event) {
+            if (event) {
+                event.preventDefault();
+            }
+            this.trigger('done');
+            this.remove();
+        }
+    });
+    
+    var HomepageView = TemplateView.extend({
+        templateName: '#home-template'
+    });
+    
+    var LoginView = FormView.extend({
+        id: 'login',
+        templateName: '#login-template',
+        submit: function (event) {
+            var data = {};
+            FormView.prototype.submit.apply(this, arguments);
+            data = this.serializeForm(this.form);
+            $.post(app.apiLogin, data)
+                .success($.proxy(this.loginSuccess, this))
+                .fail($.proxy(this.failure, this));
+        },
+        loginSuccess: function (data) {
+            app.session.save(data.token);
+            this.done();
         }
     });
     
