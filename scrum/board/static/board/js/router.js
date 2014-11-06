3b2b54844e8b5ae9eb0ba195f2403bba9ab087f0
Julia Elman <julia.elman@gmail.com>
2014-11-05 22:45:53 -0500

    Authenticating routes to use login info for Chapter 5 scrum project.

--- scrum/board/static/board/js/router.js
+++ scrum/board/static/board/js/router.js
@@ -13,8 +13,33 @@
             var view = new app.views.HomepageView({el: this.contentElement});
             this.render(view);
         },
+        route: function (route, name, callback) {
+            // Override default route to enforce login on every page
+            var login;
+            callback = callback || this[name];
+            callback = _.wrap(callback, function (original) {
+                var args = _.without(arguments, original);
+                if (app.session.authenticated()) {
+                    original.apply(this, args);
+                } else {
+                    // Show the login screen before calling the view
+                    $(this.contentElement).hide();
+                    // Bind original callback once the login is successful
+                    login = new app.views.LoginView();
+                    $(this.contentElement).after(login.el);
+                    login.on('done', function () {
+                        $(this.contentElement).show();
+                        original.apply(this, args);
+                    }, this);
+                    // Render the login form
+                    login.render();
+                }
+            });
+            return Backbone.Router.prototype.route.apply(this, [route, name, callback]);
+        },
         render: function (view) {
             if (this.current) {
+                this.current.undelegateEvents();
                 this.current.$el = $();
                 this.current.remove();
             }
