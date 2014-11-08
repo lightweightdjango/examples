31d5fcf9763f749b99f667931de4bf70592b69ab
mlavin <markdlavin@gmail.com>
2014-11-07 20:32:37 -0500

    Inline edit of task details.

--- scrum/board/static/board/js/views.js
+++ scrum/board/static/board/js/views.js
@@ -210,6 +210,9 @@
         tagName: 'div',
         className: 'task-detail',
         templateName: '#task-detail-template',
+        events: _.extend({
+            'blur [data-field][contenteditable=true]': 'editField'
+        }, FormView.prototype.events),
         initialize: function (options) {
             FormView.prototype.initialize.apply(this, arguments);
             this.task = options.task;
@@ -232,6 +235,31 @@
         success: function (model) {
             this.changes = {};
             $('button.save', this.$el).hide();
+        },
+        editField: function (event) {
+            var $this = $(event.currentTarget),
+                value = $this.text().replace(/^\s+|\s+$/g,''),
+                field = $this.data('field');
+            this.changes[field] = value;
+            $('button.save', this.$el).show();
+        },
+        showErrors: function (errors) {
+            _.map(errors, function (fieldErrors, name) {
+                var field = $('[data-field=' + name + ']', this.$el);
+                if (field.length === 0) {
+                    field = $('[data-field]', this.$el).first();
+                }
+                function appendError(msg) {
+                    var parent = field.parent('.with-label'),
+                        error = this.errorTemplate({msg: msg});
+                    if (parent.length  === 0) {
+                        field.before(error);
+                    } else {
+                        parent.before(error);
+                    }
+                }
+                _.map(fieldErrors, appendError, this);
+            }, this);
         }
     });
 
