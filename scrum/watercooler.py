b918642af2e67fceb1d84a0b1f3ca16dd9f375d9
mlavin <markdlavin@gmail.com>
2014-11-08 08:44:26 -0500

    Adding command line options and shutdown.

--- scrum/watercooler.py
+++ scrum/watercooler.py
@@ -1,8 +1,20 @@
+import logging
+import signal
+import time
+
 from urllib.parse import urlparse
 
+from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado.options import define, parse_command_line, options
 from tornado.web import Application
 from tornado.websocket import WebSocketHandler
+
+
+define('debug', default=False, type=bool, help='Run in debug mode')
+define('port', default=8080, type=int, help='Server port')
+define('allowed_hosts', default="localhost:8080", multiple=True,
+       help='Allowed hosts for cross domain connections')
 
 
 class SprintHandler(WebSocketHandler):
@@ -11,7 +23,8 @@ class SprintHandler(WebSocketHandler):
     def check_origin(self, origin):
         allowed = super().check_origin(origin)
         parsed = urlparse(origin.lower())
-        return allowed or parsed.netloc.startswith('localhost:')
+        matched = any(parsed.netloc == host for host in options.allowed_hosts)
+        return options.debug or allowed or matched
 
     def open(self, sprint):
         """Subscribe to sprint updates on a new connection."""
@@ -23,9 +36,25 @@ class SprintHandler(WebSocketHandler):
         """Remove subscription."""
 
 
+def shutdown(server):
+    ioloop = IOLoop.instance()
+    logging.info('Stopping server.')
+    server.stop()
+
+    def finalize():
+        ioloop.stop()
+        logging.info('Stopped.')
+
+    ioloop.add_timeout(time.time() + 1.5, finalize)
+
+
 if __name__ == "__main__":
+    parse_command_line()
     application = Application([
         (r'/(?P<sprint>[0-9]+)', SprintHandler),
-    ])
-    application.listen(8080)
+    ], debug=options.debug)
+    server = HTTPServer(application)
+    server.listen(options.port)
+    signal.signal(signal.SIGINT, lambda sig, frame: shutdown(server))
+    logging.info('Starting server on localhost:{}'.format(options.port))
     IOLoop.instance().start()
