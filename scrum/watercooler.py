371564fcac70926cd759a9305f1216c40cbf7e52
mlavin <markdlavin@gmail.com>
2014-11-08 14:59:18 -0500

    Using Redis Pub/Sub rather than in memory subscriber dictionary.

--- scrum/watercooler.py
+++ scrum/watercooler.py
@@ -2,21 +2,48 @@ import json
 import logging
 import signal
 import time
+import uuid
 
 from collections import defaultdict
 from urllib.parse import urlparse
 
+from redis import Redis
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.options import define, parse_command_line, options
 from tornado.web import Application, RequestHandler
-from tornado.websocket import WebSocketHandler, WebSocketClosedError
+from tornado.websocket import WebSocketHandler
+from tornadoredis import Client
+from tornadoredis.pubsub import BaseSubscriber
 
 
 define('debug', default=False, type=bool, help='Run in debug mode')
 define('port', default=8080, type=int, help='Server port')
 define('allowed_hosts', default="localhost:8080", multiple=True,
        help='Allowed hosts for cross domain connections')
+
+
+class RedisSubscriber(BaseSubscriber):
+
+    def on_message(self, msg):
+        """Handle new message on the Redis channel."""
+        if msg and msg.kind == 'message':
+            try:
+                message = json.loads(msg.body)
+                sender = message['sender']
+                message = message['message']
+            except (ValueError, KeyError):
+                message = msg.body
+                sender = None
+            subscribers = list(self.subscribers[msg.channel].keys())
+            for subscriber in subscribers:
+                if sender is None or sender != subscriber.uid:
+                    try:
+                        subscriber.write_message(message)
+                    except tornado.websocket.WebSocketClosedError:
+                        # Remove dead peer
+                        self.unsubscribe(msg.channel, subscriber)
+        super().on_message(msg)
 
 
 class SprintHandler(WebSocketHandler):
@@ -31,6 +58,7 @@ class SprintHandler(WebSocketHandler):
     def open(self, sprint):
         """Subscribe to sprint updates on a new connection."""
         self.sprint = sprint
+        self.uid = uuid.uuid4().hex
         self.application.add_subscriber(self.sprint, self)
 
     def on_message(self, message):
@@ -72,30 +100,23 @@ class ScrumApplication(Application):
             (r'/(?P<model>task|sprint|user)/(?P<pk>[0-9]+)', UpdateHandler),
         ]
         super().__init__(routes, **kwargs)
-        self.subscriptions = defaultdict(list)
+        self.subscriber = RedisSubscriber(Client())
+        self.publisher = Redis()
 
     def add_subscriber(self, channel, subscriber):
-        self.subscriptions[channel].append(subscriber)
+        self.subscriber.subscribe(['all', channel], subscriber)
 
     def remove_subscriber(self, channel, subscriber):
-        self.subscriptions[channel].remove(subscriber)
-
-    def get_subscribers(self, channel):
-        return self.subscriptions[channel]
+        self.subscriber.unsubscribe(channel, subscriber)
+        self.subscriber.unsubscribe('all', subscriber)
 
     def broadcast(self, message, channel=None, sender=None):
-        if channel is None:
-            for c in self.subscriptions.keys():
-                self.broadcast(message, channel=c, sender=sender)
-        else:
-            peers = self.get_subscribers(channel)
-            for peer in peers:
-                if peer != sender:
-                    try:
-                        peer.write_message(message)
-                    except WebSocketClosedError:
-                        # Remove dead peer
-                        self.remove_subscriber(channel, peer)
+        channel = 'all' if channel is None else channel
+        message = json.dumps({
+            'sender': sender and sender.uid,
+            'message': message
+        })
+        self.publisher.publish(channel, message)
 
 
 def shutdown(server):
