bbf88e20765ede522cd2026e08e2a24c7da33b89
mlavin <markdlavin@gmail.com>
2015-01-17 16:18:23 -0500

    Errata 151149: Update field validation methods to match changes in Django REST Framework 3.0.X

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -29,14 +29,14 @@ class SprintSerializer(serializers.ModelSerializer):
             	request=request) + '?sprint={}'.format(obj.pk),
         }
         
-    def validate_end(self, attrs, source):
-        end_date = attrs[source]
-        new = not self.object
-        changed = self.object and self.object.end != end_date
-        if (new or changed) and (end_date < date.today()):
+    def validate_end(self, value):
+        new = self.instance is None
+        changed = self.instance and self.instance.end != value
+        if (new or changed) and (value < date.today()):
             msg = _('End date cannot be in the past.')
             raise serializers.ValidationError(msg)
-        return attrs
+        return value
+
 
 class TaskSerializer(serializers.ModelSerializer):
     
@@ -71,21 +71,20 @@ class TaskSerializer(serializers.ModelSerializer):
                 kwargs={User.USERNAME_FIELD: obj.assigned}, request=request)
         return links
     
-    def validate_sprint(self, attrs, source):
-        sprint = attrs[source]
-        if self.object and self.object.pk:
-            if sprint != self.object.sprint:
-                if self.object.status == Task.STATUS_DONE:
+    def validate_sprint(self, value):
+        if self.instance and self.instance.pk:
+            if value != self.instance.sprint:
+                if self.instance.status == Task.STATUS_DONE:
                     msg = _('Cannot change the sprint of a completed task.')
                     raise serializers.ValidationError(msg)
-                if sprint and sprint.end < date.today():
+                if value and value.end < date.today():
                     msg = _('Cannot assign tasks to past sprints.')
                     raise serializers.ValidationError(msg)
         else:
-            if sprint and sprint.end < date.today():
+            if value and value.end < date.today():
                 msg = _('Cannot add tasks to past sprints.')
                 raise serializers.ValidationError(msg)
-        return attrs
+        return value
 
     def validate(self, attrs):
         sprint = attrs.get('sprint')
@@ -101,7 +100,8 @@ class TaskSerializer(serializers.ModelSerializer):
         if completed and status != Task.STATUS_DONE:
             msg = _('Completed date cannot be set for uncompleted tasks.')
             raise serializers.ValidationError(msg)
-        return attrs    
+        return attrs
+
 
 class UserSerializer(serializers.ModelSerializer):
 	
