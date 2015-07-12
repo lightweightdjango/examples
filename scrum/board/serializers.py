ab0f6e83742e3aa108c2653a3533153583cccddf
mlavin <markdlavin@gmail.com>
2015-07-12 12:52:20 -0400

    Need to explicitly allow_null for the assigned user.

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -41,7 +41,7 @@ class SprintSerializer(serializers.ModelSerializer):
 class TaskSerializer(serializers.ModelSerializer):
 
     assigned = serializers.SlugRelatedField(
-        slug_field=User.USERNAME_FIELD, required=False,
+        slug_field=User.USERNAME_FIELD, required=False, allow_null=True,
         queryset=User.objects.all())
     status_display = serializers.SerializerMethodField()
     links = serializers.SerializerMethodField()
